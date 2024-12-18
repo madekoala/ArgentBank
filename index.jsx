1829bb9a213c57f6906f5b7c70b2e038a940a942
Cresson Jeremy <jrmcresson@gmail.com>
2024-12-18 17:19:56 +0100

    deplacement fichier + supression com

--- index.jsx
+++ index.jsx
@@ -13,21 +13,14 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  // Utilisation du composant Provider de react-redux pour fournir le store Redux à l'application
   <Provider store={store}>
     {/* Utilisation de React.StrictMode pour détecter les problèmes potentiels dans l'application */}
     <React.StrictMode>
-      {/* Utilisation du Router pour gérer les routes de l'application */}
       <Router history={history}>
-        {/* Utilisation de Routes pour définir les différentes routes de l'application */}
         <Routes>
-          {/* Route pour la page d'accueil */}
           <Route path="/" element={<Accueil />} />
-          {/* Route pour la page de connexion */}
           <Route path="/Login" element={<Connexion />} />
-          {/* Route pour la page de profil utilisateur */}
           <Route path="/Profile/*" element={<User />} />
-          {/* Route par défaut en cas d'URL inconnue */}
           <Route path="/*" element={<Connexion />} />
         </Routes>
       </Router>
