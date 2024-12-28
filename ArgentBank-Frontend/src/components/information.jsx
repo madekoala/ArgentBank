ccaa5594bd19f80a04739aca81a0dd511de44479
Cresson Jeremy <jrmcresson@gmail.com>
2024-12-28 21:50:56 +0100

    Creation des end points / action update + data + deconnexion

--- ArgentBank-Frontend/src/components/information.jsx
+++ ArgentBank-Frontend/src/components/information.jsx
@@ -1,76 +1,54 @@
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
-import { updateUserInfo } from '../redux/action'
-import store from '../redux/store'
-import React from 'react'
-
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { updateUserInfo } from "../redux/action";
+import store from "../redux/store";
+import React from "react";
 
 function Information() {
-  // État local pour gérer le mode d'édition
-  const [editMode, setEditMode] = useState(false)
-  // État local pour stocker le nouveau nom d'utilisateur en cours de modification
-  const [newUserName, setNewUserName] = useState('')
-  // Utilisation du hook useSelector pour accéder aux informations de l'utilisateur depuis le store Redux
-  const userName = useSelector((state) => state.user.userName)
-  const prenom = useSelector((state) => state.user.prenom)
-  const nom = useSelector((state) => state.user.nom)
+  const [editMode, setEditMode] = useState(false);
+  const [newUserName, setNewUserName] = useState("");
+  const userName = useSelector((state) => state.user.userName);
+  const prenom = useSelector((state) => state.user.prenom);
+  const nom = useSelector((state) => state.user.nom);
 
   function handleSave() {
-    // Vérifie si le nouveau nom d'utilisateur n'est pas vide avant de le sauvegarder
-    if (newUserName !== '') {
-      // Dispatch de l'action pour mettre à jour les informations de l'utilisateur
-      store.dispatch(updateUserInfo(newUserName))
-      // Sortir du mode édition après la sauvegarde
-      setEditMode(false)
-      // Afficher une alerte indiquant que le changement a été effectué avec succès
-      alert('Changement effectué avec succès')
+    if (newUserName !== "") {
+      store.dispatch(updateUserInfo(newUserName));
+      setEditMode(false);
+      alert("Changement effectué avec succès");
     }
   }
 
   function handleCancel() {
-    // Sortir du mode édition sans sauvegarder les changements
-    setEditMode(false)
+    setEditMode(false);
   }
 
   function editData() {
-    // Pré-remplit le champ d'édition avec le nom d'utilisateur actuel
-    setNewUserName(userName)
-    // Activer le mode édition
-    setEditMode(true)
+    setNewUserName(userName);
+    setEditMode(true);
   }
 
   return (
     <div className="information">
       {editMode ? (
-        // Formulaire d'édition
         <div>
           <h1>Edit user info</h1>
           <form>
-            <div class="user-name">
-              <label for="username">User name</label>
+            <div className="user-name">
+              <label htmlFor="username">User name</label>
               <input
                 type="text"
                 id="username"
                 onChange={(e) => setNewUserName(e.target.value)}
               />
             </div>
-            <div class="user-name">
-              <label for="username">First name</label>
-              <input
-                type="text"
-                id="username"
-                placeholder={prenom}
-                disabled
-              />
+            <div className="user-name">
+              <label htmlFor="prenom">First name</label>
+              <input type="text" id="prenom" placeholder={prenom} disabled />
             </div>
-            <div class="user-name">
-              <label for="username">Last name</label>
-              <input
-                type="text"
-                id="username"
-                placeholder={nom}
-                disabled
-              />
+            <div className="user-name">
+              <label htmlFor="nom">Last name</label>
+              <input type="text" id="nom" placeholder={nom} disabled />
             </div>
             <div className="divButton">
               <button type="button" onClick={handleSave}>
@@ -83,7 +61,6 @@ function Information() {
           </form>
         </div>
       ) : (
-        // Affichage des informations utilisateur
         <div>
           <h1>
             Welcome <br /> {userName}
@@ -94,7 +71,7 @@ function Information() {
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Information
+export default Information;
--- ArgentBank-Frontend/src/redux/action.js
+++ ArgentBank-Frontend/src/redux/action.js
@@ -1,186 +1,116 @@
-
 /* eslint-disable no-redeclare */
 /* global fetch */
 /* eslint no-undef: "error" */
 
-import store from './store'
+import store from "./store";
 
-// Actions Redux pour modifier l'état de l'application en fonction des requêtes
+// Actions Redux pour modifier l'état de l'application
+const dataFetching = () => ({ type: "loading" });
+const dataError = () => ({ type: "error" });
+const connexionAction = (data) => ({ type: "connexion", payload: data });
+const profileAction = (data) => ({ type: "profile", payload: data });
+const updateUserAction = (data) => ({ type: "updateUser", payload: data });
+const deconnexionAction = () => ({ type: "deconnexion" });
 
-/**
- * Fonction dataFetching
- * Indique que des données sont en cours de chargement.
- * @returns {Object} - Action Redux indiquant que des données sont en cours de chargement.
- */
-const dataFetching = () => ({ type: 'loading' })
-
-/**
- * Fonction dataError
- * Indique qu'une erreur s'est produite lors du chargement des données.
- * @returns {Object} - Action Redux indiquant qu'une erreur s'est produite.
- */
-const dataError = () => ({ type: 'error' })
-
-/**
- * Fonction connexionAction
- * Met à jour l'état avec le jeton d'accès reçu lors de la connexion.
- * @param {Object} data - Données reçues lors de la connexion (contenant le jeton d'accès).
- * @returns {Object} - Action Redux avec les données de connexion.
- */
-const connexionAction = (data) => ({ type: 'connexion', payload: data })
-
-/**
- * Fonction profileAction
- * Met à jour l'état avec les informations du profil de l'utilisateur.
- * @param {Object} data - Données du profil de l'utilisateur.
- * @returns {Object} - Action Redux avec les informations du profil de l'utilisateur.
- */
-const profileAction = (data) => ({ type: 'profile', payload: data })
-
-/**
- * Fonction updateUserAction
- * Met à jour l'état avec les nouvelles informations de l'utilisateur (prénom, nom).
- * @param {Object} data - Nouvelles informations de l'utilisateur.
- * @returns {Object} - Action Redux avec les nouvelles informations de l'utilisateur.
- */
-const updateUserAction = (data) => ({ type: 'updateUser', payload: data })
-
-/**
- * Fonction deconnexionAction
- * Change l'état de connexion en non connecté.
- * @returns {Object} - Action Redux indiquant la déconnexion de l'utilisateur.
- */
-const deconnexionAction = () => ({ type: 'deconnexion' })
-
-// Actions Redux Thunk pour effectuer des opérations asynchrones
-
-/**
- * Fonction deconnexion
- * Change l'état de connexion en non connecté.
- * @returns {Function} - Fonction de rappel redux thunk.
- */
+// **Action Thunk : Déconnexion**
 export function deconnexion() {
   return function (dispatch) {
-    // Dispatch de l'action de déconnexion
-    dispatch(deconnexionAction())
-  }
+    dispatch(deconnexionAction());
+  };
 }
 
-/**
- * Fonction login
- * Effectue une requête pour se connecter avec un email et un mot de passe.
- * Si les paramètres sont corrects, la requête renvoie un jeton d'accès.
- * Utilise ensuite le jeton pour appeler la fonction profile.
- * @param {String} email - Email du compte.
- * @param {String} password - Mot de passe du compte.
- * @returns {Function} - Fonction de rappel redux thunk.
- */
+// **Action Thunk : Connexion**
 export function login(email, password) {
-  return function (dispatch) {
+  return async function (dispatch) {
     try {
-      // Dispatch de l'action indiquant le chargement des données
-      dispatch(dataFetching())
+      dispatch(dataFetching()); // Début du chargement
 
-      // Options de requête pour la connexion
       const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json; charset=UTF-8' },
-        body: JSON.stringify({ "email": email, "password": password })
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify({ email, password }),
+      };
+
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/login",
+        requestOptions
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(connexionAction(data)); // Met à jour le token
+        dispatch(profile()); // Récupère le profil
+      } else {
+        throw new Error(`Erreur ${response.status}: ${response.statusText}`);
       }
-
-      // Appel de l'API pour la connexion
-      fetch('http://localhost:3001/api/v1/user/login', requestOptions)
-        .then(function (response) {
-          if (response.ok) {
-            console.log("Erreur de réponse :", response);
-            response.json().then(function (data) {
-              // Dispatch de l'action avec les données de connexion
-              dispatch(connexionAction(data))
-
-              // Appel de la fonction profile pour récupérer les informations du profil de l'utilisateur
-              dispatch(profile())
-            })
-          } else {
-            // En cas d'erreur de connexion, dispatch de l'action d'erreur
-            dispatch(dataError())
-          }
-        })
     } catch (error) {
-      // En cas d'erreur inattendue, dispatch de l'action d'erreur
-      dispatch(dataError())
+      console.error("Erreur dans login:", error.message);
+      dispatch(dataError());
     }
-  }
+  };
 }
 
-/**
- * Fonction profile
- * Effectue une requête pour récupérer les informations de profil de l'utilisateur à partir du jeton d'accès.
- * @returns {Function} - Fonction de rappel redux thunk.
- */
+// **Action Thunk : Récupération du Profil**
 export function profile() {
-  return function (dispatch) {
+  return async function (dispatch) {
     try {
-      // Options de requête pour récupérer le profil de l'utilisateur
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-          Authorization: 'Bearer ' + store.getState().token
-        }
-      }
+      dispatch(dataFetching()); // Début du chargement
 
-      // Appel de l'API pour récupérer le profil de l'utilisateur
-      fetch('http://localhost:3001/api/v1/user/profile', requestOptions)
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              // Dispatch de l'action avec les informations du profil de l'utilisateur
-              dispatch(profileAction(data))
-            })
-          }
-        })
+      const requestOptions = {
+        method: "GET", // Utilisation de GET pour récupérer les données
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${store.getState().token}`, // Inclusion du token
+        },
+      };
+
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/profile",
+        requestOptions
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(profileAction(data)); // Mise à jour avec les données du profil
+      } else {
+        throw new Error(`Erreur ${response.status}: ${response.statusText}`);
+      }
     } catch (error) {
-      // En cas d'erreur inattendue, dispatch de l'action d'erreur
-      dispatch(dataError())
+      console.error("Erreur dans profile:", error.message);
+      dispatch(dataError());
     }
-  }
+  };
 }
 
-/**
- * Fonction updateUserInfo
- * Met à jour les informations de l'utilisateur (prénom, nom) dans la base de données.
- * @param {String} userName - Nom d'utilisateur à mettre à jour.
- * @returns {Function} - Fonction de rappel redux thunk.
- */
+// **Action Thunk : Mise à Jour des Informations de l'Utilisateur**
 export function updateUserInfo(userName) {
-  return function (dispatch) {
+  return async function (dispatch) {
     try {
-      // Dispatch de l'action indiquant le chargement des données
-      dispatch(dataFetching())
+      dispatch(dataFetching()); // Début du chargement
 
-      // Options de requête pour mettre à jour les informations de l'utilisateur
       const requestOptions = {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-          Authorization: 'Bearer ' + store.getState().token
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${store.getState().token}`, // Inclusion du token
         },
-        body: JSON.stringify({ "userName": userName })
-      }
+        body: JSON.stringify({ userName }),
+      };
 
-      // Appel de l'API pour mettre à jour les informations de l'utilisateur
-      fetch("http://localhost:3001/api/v1/user/profile", requestOptions)
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              // Dispatch de l'action avec les nouvelles informations de l'utilisateur
-              dispatch(updateUserAction(data))
-            })
-          }
-        })
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/profile",
+        requestOptions
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(updateUserAction(data)); // Mise à jour du profil
+      } else {
+        throw new Error(`Erreur ${response.status}: ${response.statusText}`);
+      }
     } catch (error) {
-      // En cas d'erreur inattendue, dispatch de l'action d'erreur
-      dispatch(dataError())
+      console.error("Erreur dans updateUserInfo:", error.message);
+      dispatch(dataError());
     }
-  }
+  };
 }
